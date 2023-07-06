f51544438e9c14750e56162ef0efba6256e99d69
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-06 23:41:25 +0000

    Thu, Jul 6, 2023, 8:41 PM -03:00

--- mario/mario.c
+++ mario/mario.c
@@ -6,7 +6,7 @@ int main(void)
     int s = 0
     int b = 0
     int a = get_int ("Digite a altura da piramide que deseja(aceitavel entre 1 e 8)");
-    if 
+    if ( a > 0 )
     for (int i = 0; i < a; i++)
     {
        for (int j = 0; j < a; j++)
