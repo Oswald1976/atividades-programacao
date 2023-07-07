84e634bf56efdf2b00565ccda27113fc2123a027
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-07 09:41:48 +0000

    Fri, Jul 7, 2023, 6:41 AM -03:00

--- cash/cash.c
+++ cash/cash.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 
 float cent;
-int m25 = 0, m10 = 0, 
-float c = 0.10;
-float d = 0.05;
-float e = 0.01;
+int m25 = 0, m10 = 0, m5 = 0, m1 = 0;
 
 int main(void)
 {
     do
         cent = get_float("Digite o Valor que terá que dar de troco em moedas (aceitavel entre 0,01 e 0,99)");
-    while ((float) cent < 0, 01 || (float) cent > 0, 99);
+    while ((float) cent < 0,01 || (float) cent > 0,99);
     {
 
     }
