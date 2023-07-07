b6dc8a3986198540784871de352f80e6f063706b
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-07 09:08:26 +0000

    Fri, Jul 7, 2023, 6:08 AM -03:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int a;
 int main(void)
-{
+{   do
+    a = get_flot("Digite o Valor que terá que dar de troco em moedas (aceitavel entre 0,01 e 0,99)");
+    while (a < 0,01 || a > 0,99)
+
     
 }
