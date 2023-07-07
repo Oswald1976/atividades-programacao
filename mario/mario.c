26ed30f2e2a580631ad03c370716a13148d8927d
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-07 07:04:15 +0000

    Fri, Jul 7, 2023, 4:04 AM -03:00

--- mario/mario.c
+++ mario/mario.c
@@ -5,10 +5,12 @@ int main(void)
 {
     int s = 0;
     int b = 0;
-    int a = get_int ("Digite a altura da piramide que deseja(aceitavel entre 1 e 8)");
+    do
+    {
+        int a = get_int ("Digite a altura da piramide que deseja(aceitavel entre 1 e 8)");
+    }
     while ( a > 0 || a < 9 )
-        {
-            for (int i = 0; i < a; i++)
+        for (int i = 0; i < a; i++)
             {
                 for (int j = 0; j < a; j++)
                 {
@@ -25,8 +27,4 @@ int main(void)
             printf("\n");
             }
 
-        }
-
-              printf("ATENÇÃO O NUMERO DEVE ESTAR ENTRE 1 E 8 \n");
-          
 }
