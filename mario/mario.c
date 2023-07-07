6bdd7d4d055b3ce763b37ec958bd89218dbbd6db
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-07 07:43:03 +0000

    Fri, Jul 7, 2023, 4:43 AM -03:00

--- mario/mario.c
+++ mario/mario.c
@@ -15,9 +15,7 @@ int main(void)
             {
                 for (int j = 0; j < a; j++)
                 {
-
                         printf("#");
-                       
                 }
             printf("\n");
             }
