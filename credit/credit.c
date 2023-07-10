44138e09da4165224fc8f4b030d5cbff74cb8b9d
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-10 09:27:24 +0000

    Mon, Jul 10, 2023, 6:27 AM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,16 +2,16 @@
 #include <cs50.h>
 
 // Prototype the functions
-long function1(long n);
-long function2(long n);
-long cardNumValidation(long n)
+// long function1(long n);
+// long function2(long n);
+//long cardNumValidation(long n)
 
 int main(void)
 {
     do
     {
-    n = get_long("Digite o Numero do Cartão de Credito .. : ");
+    numerocartao = get_long("Digite o Numero do Cartão de Credito .. : ");
     }
-    while (n > 1);
-    
+    while (numerocartao > 1);
+pr
 }
