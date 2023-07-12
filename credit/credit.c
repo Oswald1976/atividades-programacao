07a65a503d87079944a7252dce4ff7e1275de908
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-12 00:58:46 +0000

    Tue, Jul 11, 2023, 9:58 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -17,7 +17,7 @@ int main(void)
 
 
     printf("Digite o Numero do Cartão de Credito .. : ");
-    scanf( "%c", &numerocartao);
+    scanf( "%s", &numerocartao);
 
     if (numerocartao < 1)
     {
