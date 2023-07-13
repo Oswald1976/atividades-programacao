c81209855e0e065762e54082467d0ad699cfebd6
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-13 02:00:26 +0000

    Wed, Jul 12, 2023, 11:00 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -20,7 +20,7 @@ int main(void)
     printf("Digite o Numero do Cartão de Credito .. : ");
     scanf(" %s", nrcartao);
 
-    for(i = 0; i <= strlen(nrcartao); i++)
+    for(i = 0; i < strlen(nrcartao); i++)
     {
         if(nrcartao[i] != '0' && nrcartao[i] != '1' &&
         nrcartao[i] != '2' && nrcartao[i] != '3' &&
@@ -44,7 +44,7 @@ int main(void)
 
 
         //Efetua a conversao de array de char para um array de int.
-        for(i=0; i <= strlen(nrcartao) ;i++) // strlen = retorna a quantidade de elemento no array
+        for(i=0; i < strlen(nrcartao) ;i++) // strlen = retorna a quantidade de elemento no array
         {
             inrcartao[i]=nrcartao[i]-48; // pq -48 ?
             qtd++; // Acumula  quantidade de elementos do array
