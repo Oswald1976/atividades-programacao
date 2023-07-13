f7050e993c76d07bd194c3837ce7750c5f5e3a32
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-13 01:29:43 +0000

    Wed, Jul 12, 2023, 10:29 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -18,7 +18,7 @@ int main(void)
     printf("Digite o Numero do Cartão de Credito .. : ");
     scanf(" %s", nrcartao);
 
-    whilefor(i = 0; i <= strlen(nrcartao); i++)
+    for(i = 0; i <= strlen(nrcartao); i++)
     {
         if(nrcartao[i] != '0' && nrcartao[i] != '1' &&
         nrcartao[i] != '2' && nrcartao[i] != '3' &&
