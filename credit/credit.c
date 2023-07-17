b497f239dc0fd94fba2109ffea933187a8d3dc72
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-17 18:07:51 +0000

    Mon, Jul 17, 2023, 3:07 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -28,26 +28,58 @@ int main(void)
         nrcartao[i] != '\0')
         {
             printf("INVALID\n");
-            printf("\n");
+/*            printf("\n");
             printf("ERRO !!!, Lembre de digitar apenas numeros \n");
             printf("\n");
             printf("TENTE NOVAMENTE.... \n");
-            printf("\n");
+            printf("\n"); */
+            return 0;
         }
     }
 
-   /* if (len < 13 || len  > 16) // confere se a quantidade numeros esta na faixa aceitavel
+    if (len < 13 || len  > 16) // confere se a quantidade numeros esta na faixa aceitavel
     {
         printf("INVALID");
         return 0;
     }
 
-*/
-
     for(i=0; i < len  ;i++) //
     {
         inrcartao[i]=nrcartao[i]-48; // pq -48 ?  Efetua a conversao de array de char para um array de int.
     }
+
+
+
+
+ // int validarCartaoCredito(const char* numeroCartao) { PODE VIRAR UMA FUNÇAO POSTERIORMENTE
+
+    for (i = len - 2; i >= 0; i -= 2) // Inicia a partir do penúltimo para o primeiro
+    {
+
+        if ((inrcartao[i] * 2) > 9) // Multiplica por 2 e verifica se o resultado é maior que 9
+
+        {
+            inrmulti += (inrcartao[i] * 2) - 9;
+        }
+        else
+        {
+            inrmulti += inrcartao[i] * 2;
+        }
+    }
+
+
+    for (i = len - 1; i >= 0; i -= 2) // Soma os dígitos que nao foram ultilizados na operação anterior
+    {
+        inrnorma += inrcartao[i];
+
+    }
+
+    soma = inrmulti + inrnorma;
+
+
+    if (soma % 10 == 0) // Verifica se o número é válido
+    {
+
 
 // testa os codigos para saber qual bandeira pertence o cartão
         if(nrcartao[0] == '3' && nrcartao[1] == '4')
@@ -85,39 +117,11 @@ int main(void)
         else
         {
            printf("INVALID\n");
-          // return 0;
+           return 0;
         }
 
 
- // int validarCartaoCredito(const char* numeroCartao) { PODE VIRAR UMA FUNÇAO POSTERIORMENTE
 
-    for (i = len - 2; i >= 0; i -= 2) // Inicia a partir do penúltimo para o primeiro
-    {
-
-        if ((inrcartao[i] * 2) > 9) // Multiplica por 2 e verifica se o resultado é maior que 9
-
-        {
-            inrmulti += (inrcartao[i] * 2) - 9;
-        }
-        else
-        {
-            inrmulti += inrcartao[i] * 2;
-        }
-    }
-
-
-    for (i = len - 1; i >= 0; i -= 2) // Soma os dígitos que nao foram ultilizados na operação anterior
-    {
-        inrnorma += inrcartao[i];
-
-    }
-
-    soma = inrmulti + inrnorma;
-
-
-    if (soma % 10 == 0) // Verifica se o número é válido
-    {
-        printf("VALID\n"); // Válido
     } else {
         printf("INVALID\n"); // Inválido
         return 0;
