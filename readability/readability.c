029d68991ab51ef848e534103c593cc5e06794b4
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-22 06:30:06 +0000

    Sat, Jul 22, 2023, 3:30 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 
-string texto[];
+string texto;
 int i, len , letras = 0 , palav = 0, frase = 0;
 int main(void)
 {
     printf("Texto");
     scanf("%s", texto);
     for(i = 0, len = strlen(texto); i < len ; i++)
-        if(texto[i] >= 'a' && texto[i] <= 'z') // || texto[i] >= 'A' && texto[i] <= 'Z')
+        if(texto[i] >= 'a' && texto[i] <= 'z')  || texto[i] >= 'A' && texto[i] <= 'Z')
 
         {
             letras++;
