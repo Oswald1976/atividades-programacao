d98863e077a551ece273098f11011c8e043215fc
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-22 00:39:22 +0000

    Fri, Jul 21, 2023, 9:39 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,7 +7,7 @@ int i, len, letras, palav, frase;
 int main(void)
 {
     printf("Texto");
-    scanf(%c, texto);
+    scanf(%i, texto);
     for(i = 0, len = strlen(texto); i < len ; i++)
         if(texto[i] >= 'a' && texto[i] <= 'z' || texto[i] >= 'A' && texto[i] <= 'Z')
         {
