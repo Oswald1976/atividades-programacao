7b2923ec1fa87fdb78a329c15143bd58a4508eb0
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-22 08:27:08 +0000

    Sat, Jul 22, 2023, 5:27 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -35,9 +35,9 @@ int main(void)
     }while( i < len );
 
     float L = letras / (float) palavr * 100;
-    float L = letras / (float) palavr * 100;
+    float L = frase / (float) palavr * 100;
 
-    //INDIC = 0,0588 * L - 0,296 * S - 15,8
+    float INDIC = (0,0588 * L) - (0,296 * S - 15,8);
 
 
 
