1dbabe32da734d1e544b5372dca93a01973a1eb2
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-26 02:29:49 +0000

    Tue, Jul 25, 2023, 11:29 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,19 +10,17 @@ int main(int argc, char *argv[])
 {
     char argvc[2];
 
-    if(argc != 2)
+    if(argc == 2)
     {
-        printf("Usage: ./caesar key \n");
-    }
+        for(int i=0, len=strlen(argv[1]); i < len; i++)
+            {
+                argvc[i] = argv{1,i};
+            }
 
-    for(int i=0, len=strlen(argv[1]); i < len; i++)
-        {
-            argvc[i] = argv{1,i};
-        }
-
-    if(isdigit(argvc[1]))
-    {
-        printf("Esta certo vc digitou um numero decimal \n");
+         if(isdigit(argvc[1]))
+            {
+                printf("Esta certo vc digitou um numero decimal \n");
+            }
     }
     else
     {
