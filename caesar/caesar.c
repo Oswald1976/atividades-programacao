d4672a82a19d3117ca681b5ff91e9f4e8b64580b
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-26 01:41:22 +0000

    Tue, Jul 25, 2023, 10:41 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,14 +1,16 @@
+#include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#include <stdlib.h>
+
+
 
 int main(int argc, string argv[])
 {
     if(argc != 2)
     {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
     }
 
 }
