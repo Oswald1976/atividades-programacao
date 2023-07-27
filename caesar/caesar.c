69592e8552392dbf9e8347f0a2a5c897b8785c70
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-27 06:25:33 +0000

    Thu, Jul 27, 2023, 3:25 AM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
                     if(isupper(txt[i])) // se for alfabeto, ai testa pra ver se é maiusculo
                     {
-                         txtcif[1]=int(txt[i])                               // se for minusculo.. faz isso
+                         txtcif[1]=int(txt[i]) + key                               // se for minusculo.. faz isso
                     }
                     else
                     {
