33bd6b01cf1307b164e156d75cac74a05c7586d0
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-27 06:47:32 +0000

    Thu, Jul 27, 2023, 3:47 AM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
 
         if(keyCheckFunc(argv[1]) == 1) // check argv[1,i] = int
         {
-            for(i=n,len=strlen(argv); i<len; i++)
-            {
+
+
                 key=atoi(argv[1]); // atoi converte char para int
-            }
+          
             printf("plaintext: ");
             scanf("%s", txt);
             for(i = 0 , len=strlen(txt) ; i < len ; i++)
