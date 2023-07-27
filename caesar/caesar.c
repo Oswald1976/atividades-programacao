875555b61b34441d7e09513ae7781e64dd4a26c9
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-27 05:46:49 +0000

    Thu, Jul 27, 2023, 2:46 AM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -19,7 +19,10 @@ int main(int argc, char *argv[])
 
         if(keyCheckFunc(argv[1]) == 1) // check argv[1,i] = int
         {
-            key[0] = (int)argv[1,0];
+            for(i=n,len=strlen(argv); i<len; i++)
+            {
+                key[0] = argv[1,0];
+            }
             printf("plaintext: ");
             scanf("%s", txt);
             for(i = 0 , len=strlen(txt) ; i < len ; i++)
