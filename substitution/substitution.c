6bc7de2605ea8c51ade588757db341b9fa0498a1
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-28 07:43:15 +0000

    Fri, Jul 28, 2023, 4:43 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,12 +3,22 @@
 #include <ctype.h>
 #include <string.h>
 
+int keycheck(char x[]);  // prototype functions
+
+
 
 int main(int argc, char *argv[])
 {
     int len=0
     if(argc == 2 ) // check two arrays ?
     {
-        
+
     }
+}
+
+// functions
+
+int keycheck(char x[])
+{
+    
 }
