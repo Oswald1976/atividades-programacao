1177a955df730051e6a563f5b7ffc6d27229d9ca
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-28 09:13:22 +0000

    Fri, Jul 28, 2023, 6:13 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -16,10 +16,17 @@ int main(int argc, char *argv[])
         {
 
         }
-    }
+        else
+        {
+           printf("Usage: ./substitution KEY \n");
+           return 1;
+
+
+        }
     else
     {
        printf("Usage: ./substitution KEY \n");
+       return 1;
     }
 }
 
@@ -29,11 +36,11 @@ int keycheck(char x[])
 {
     int stop = 1;
     int len=strlen(x);
-    if(len == 26) // check 26 char is true 
+    if(len == 26) // check 26 char is true
     {
         for(int i = 0; i < len && stop == 1; i++ )
         {
-            if(isdigit(x[1]))
+            if(isdigit(x[1])) // checa se algum dos caracteres do segundo argumento é numerico
                 {
                     stop = 0;
                 }
