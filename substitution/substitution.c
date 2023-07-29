a5faa7d343f07afdba5f94723e773d83efc15015
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-29 06:11:38 +0000

    Sat, Jul 29, 2023, 3:11 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -45,9 +45,11 @@ int main(int argc, string argv[])
         for(i = 0; i < len ; i++)
         if(islower(argv[1][i]))
         {
-
+            
         }
         if(isupper(argv[1][i]))
-        
+        {
+
+        }
 
 }
