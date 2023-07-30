d5d0bbeefb4a2f7e8adef6421a475da33ea35c1c
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-30 00:56:04 +0000

    Sat, Jul 29, 2023, 9:56 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -9,7 +9,7 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 //char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-int compute_score(string word); // PROTOTIPE
+int compute_score(string word); // PROTOTYPE FUNCTION
 
 
 int main(void)
@@ -45,7 +45,7 @@ int compute_score(string word)
             int cripto = (word[i]) - 'a';
 
 
-                score += POINTS[j];
+                score += POINTS[cripto];
     }
     return score;
 }
