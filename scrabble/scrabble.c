ce07e6270e3e74a30edb9c3e6f5bc548e514b71f
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-30 02:12:58 +0000

    Sat, Jul 29, 2023, 11:12 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -49,7 +49,7 @@ int compute_score(string word)
 
     for(int i = 0; i < len; i++)
     {
-        if(isalpha)
+        if(!isalpha)
             int cripto = ((int)word[i]) - 'a';
                 score += POINTS[cripto];
     }
