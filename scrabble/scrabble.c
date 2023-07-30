583368127da5c0313b0807b8511f7713fbe63b09
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-30 00:47:09 +0000

    Sat, Jul 29, 2023, 9:47 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -15,7 +15,7 @@ int compute_score(string word);
 int main(void)
 {
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+// char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 
     // Get input words from both players
