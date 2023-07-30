9505956cd66fe5ec802779a92e5e949a25280267
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-30 00:43:43 +0000

    Sat, Jul 29, 2023, 9:43 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -4,15 +4,20 @@
 #include <string.h>
 #include <ctype.h>
 
-
+/*
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
+*/
 int compute_score(string word);
+
 
 int main(void)
 {
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+
     // Get input words from both players
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
