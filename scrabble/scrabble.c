40c495696916b445a18cd34de446067de0bcea26
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-30 01:57:00 +0000

    Sat, Jul 29, 2023, 10:57 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,15 +7,12 @@
 
  // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-//char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 int compute_score(string word); // PROTOTYPE FUNCTION
 
 
 int main(void)
 {
-
-
 
     // Get input words from both players
     string word1 = get_string("Player 1: ");
@@ -25,14 +22,20 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    iF(score1 > score2)
+    // TODO: Print the winner
+    if(score1 > score2)
     {
-        printf("Player 1: %d \n", score1);
+        printf("Player 1 wins");
+    }
+    if(score1 < score2)
+    {
+        printf("Player 2 wins");
+    }
+   if(score1 = score2)
+    {
+        printf("Player 1 wins");
     }
 
-
-    printf("Player 2: %d \n", score2);
-    // TODO: Print the winner
 }
 
 int compute_score(string word)
