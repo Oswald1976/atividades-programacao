593a7f425522cbc2cb67c789b79b47f38d64eac3
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-07-30 00:54:06 +0000

    Sat, Jul 29, 2023, 9:54 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -4,18 +4,17 @@
 #include <string.h>
 #include <ctype.h>
 
-/*
-// Points assigned to each letter of the alphabet
+
+ // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-*/
-int compute_score(string word);
+//char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+int compute_score(string word); // PROTOTIPE
 
 
 int main(void)
 {
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-// char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
 
 
     // Get input words from both players
