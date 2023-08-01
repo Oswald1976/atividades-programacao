03de47dbb566a2eff689752f1b3b25e488995533
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-01 08:16:46 +0000

    Tue, Aug 1, 2023, 5:16 AM -03:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -127,7 +127,17 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    bool voto=false;
+    for(int i=0; i < candidate_count; i++)
+    {
+        if(strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+            voto=true;
+        }
+     }
+
+
     return false;
 }
 
