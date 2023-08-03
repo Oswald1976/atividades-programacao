8891505b71388de87d28eecb2b324450e6b52bf2
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-03 08:47:55 +0000

    Thu, Aug 3, 2023, 5:47 AM -03:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -69,7 +69,7 @@ int main(int argc, string argv[])
     {
         candidates[i].name = argv[i + 1];
         candidates[i].votes = 0;
-        candidates[i].eliminated = false;
+Alic        candidates[i].eliminated = false;
     }
 
     voter_count = get_int("Number of voters: ");
