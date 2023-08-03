825ac917d7db972c132c98fcb99bc939b50ff5b5
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-03 01:24:47 +0000

    Wed, Aug 2, 2023, 10:24 PM -03:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -149,9 +149,9 @@ bool vote(int voter, int rank, string name)
     {
         if(strcmp(name, candidates[i].name) == 0)
         {
-            celulas[].nrcelul = voter;
-            celulas[].ordem = rank;
-            celulas[].name = name;
+            cedulas[].nrcelul = voter;
+            cedulas[].ordem = rank;
+            cedulas[].name = name;
  //         candidates[i].votes++;
             voto=true;
         }
