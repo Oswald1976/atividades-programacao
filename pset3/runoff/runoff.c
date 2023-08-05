aa554540d3eb4f6f2f27b35eb5bf75e805671c2c
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-05 00:28:26 +0000

    Fri, Aug 4, 2023, 9:28 PM -03:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -32,7 +32,7 @@ typedef struct
 }
 cedula;
 
-cedula *cedulas[MAX_VOTERS];
+cedula cedulas[MAX_VOTERS];
 
 
 // Numbers of voters and candidates
@@ -151,9 +151,9 @@ bool vote(int voter, int rank, string name)
     {
         if(strcmp(name, candidates[i].name) == 0)
         {
-            ?cedulas[voter].nrcedu = voter;
-            ?cedulas[rank].ordem = rank;
-            ?cedulas[rank].name = name;
+            cedulas[voter].nrcedu = voter;
+            cedulas[rank].ordem = rank;
+            cedulas[rank].name = name;
  //         candidates[i].votes++;
             voto=true;
         }
