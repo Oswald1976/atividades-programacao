ef3a20ac225914df2ee682e368fb01fbfa2db637
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-07 00:17:31 +0000

    Sun, Aug 6, 2023, 9:17 PM -03:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -109,13 +109,13 @@ int main(int argc, string argv[])
         //rotina pra fazer o primeiro calculo dos primeiros em rank direito.
         for(int i=0, tvoto=(candidate_count * voter_count) ; i <tvoto; i+=voter_count )
         {
-            for(int j=0; j < candidate_count, j++)
+            for(int j=0; j < candidate_count; j++)
             {
-                if((strcmp(cedulas[i]name, candidates[j].name) == 0))
+                if((strcmp(cedulas[i].name, candidates[j].name) == 0))
                      candidates[j].votes++;
             }
         }
-        
+
         while(true)
         {
             // Check if election has been won
