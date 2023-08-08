f33d08af1bb77c6271a79b20f5215bd91e3766bf
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-08 06:49:11 +0000

    Tue, Aug 8, 2023, 3:49 AM -03:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -29,7 +29,7 @@ int candidate_count;
 // Function prototypes
 bool vote(int voter, int rank, string name);
 void tabulate(void);
-bool print_winner(int minvot);
+bool print_winner(void);
 int find_min(void);
 bool is_tie(int min);
 void eliminate(void);
