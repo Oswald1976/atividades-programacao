711e1b52ab33b6a2be497179144bec71538a88d5
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-23 08:59:55 +0000

    Wed, Aug 23, 2023, 5:59 AM -03:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,18 +1,22 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-     float mediaRGB;
+
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            mediaRGB = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
-            image[i][j].rgbtBlue = round(mediaRGB);
-            image[i][j].rgbtGreen = round(mediaRGB);
-            image[i][j].rgbtRed = round(mediaRGB);
+            double Blue =image[i][j].rgbtBlue;
+            double Green=image[i][j].rgbtGreen;
+            double Red  =image[i][j].rgbtRed;
+
+            int mediaRGB= round((Blue + Green +Red) / 3);
+
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = mediaRGB;
         }
     }
        return;
@@ -30,19 +34,79 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
            tmp[i][(width - 1) - j].rgbtGreen = image[i][j].rgbtGreen;
            tmp[i][(width - 1) - j].rgbtRed   = image[i][j].rgbtRed;
         }
-
-     for(int i=0; i < heidht; i++)
+    }
+     for(int i=0; i < height; i++)
      {
-        for(int j = )
+        for(int j=0; j < width; j++)
+        {
+            image[i][j].rgbtBlue  = tmp[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = tmp[i][j].rgbtGreen;
+            image[i][j].rgbtRed =   tmp[i][j].rgbtRed;
+        }
      }
 
-    }
+    free(tmp);
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE(*tmp)[width] = calloc(height, width * sizeof(RGBTRIPLE)); //
+    float media[3];
+    float divisor;
+
+    for(int i=0; i < height; i++)
+    {
+        for(int j= width-1; j >= 0; j--)
+        {
+            divisor=0;
+            for(int n=0; n < 3; n++)
+            {
+                media[n]=0;
+            }
+            for(int y=-1; y <2 ; y++)
+            {
+                for(int x=-1; x <2 ; x++)
+                {
+                    if (i + y >= 0 && i + y < height)
+                    {
+                        if (j + x >= 0 && j + x < width)
+                        {
+                            media[0] = image[i + y][j + x].rgbtBlue  + media[0];
+                            media[1] = image[i + y][j + x].rgbtGreen + media[1];
+                            media[2] = image[i + y][j + x].rgbtRed   + meida[2];
+                            divisor++;
+                        }
+                    }
+
+                }
+            }
+            if( media[0] != 0)
+            {
+                tmp[i][j].rgbtBlue = round(media[0] / divisor);
+            }
+            if( media[1] != 0)
+            {
+                tmp[i][j].rgbtBlue = round(media[1] / divisor);
+            }
+            if( media[2] != 0)
+            {
+                tmp[i][j].rgbtBlue = round(media[2] / divisor);
+            }
+        }
+     }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = tmp[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = tmp[i][j].rgbtGreen;
+            image[i][j].rgbtRed = tmp[i][j].rgbtRed;
+        }
+    }
+    free(tmp);
+
     return;
 }
 
