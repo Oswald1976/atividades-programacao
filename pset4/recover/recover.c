cf82349325e8a66c417774388f16ac14901f602a
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-26 23:05:20 +0000

    Sat, Aug 26, 2023, 8:05 PM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -33,6 +33,35 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //a função fread retorna o valor do bloco lido (512) retornando 0 quando chega ao fim do arquivo.
+    while ((imagem = fread(buffer, 1, sizeof(buffer), file)) > 0)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (gravando == 1)
+            {
+                fclose(image);
+            }
+            sprintf(img, "%03i.jpg", i);
+            image = fopen(img, "wb");
+            i++;
+            gravando = 1;
+
+        }
+
+        if (gravando == 1)
+        {
+            fwrite(buffer, 1, imagem, image);
+        }
+    }
+
+    fclose(image);
+    fclose(file);
+
+    return (0);
+
+
+
  // Ler os primeiros 3 bytes
     BYTE bytes[3];
     fread(bytes, sizeof(BYTE), 3, file);
