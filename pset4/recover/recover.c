e011f7b5e9a468138df1ec5290a0d5bfe7a63f7a
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-26 13:33:13 +0000

    Sat, Aug 26, 2023, 10:33 AM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
     if(file == NULL)
     {
-        printf("Não foi possivel abrir %O que vai ser recuperado ?!?!?!?!\n");
+        printf("Não foi possivel abrir %s !!!\n", argv[1]);
         return 1;
     }
     // Abrir o arquivo de saída
