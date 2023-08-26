2212320d2c19f860758b08c4dd3cc9ebfca6c46f
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-26 13:37:23 +0000

    Sat, Aug 26, 2023, 10:37 AM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -16,21 +16,21 @@ int main(int argc, char *argv[])
         printf("Não foi possivel abrir %s !!!\n", argv[1]);
         return 1;
     }
-    // Abrir o arquivo de saída
-     FILE *destination = fopen(argv[2], "w");
-     if (destination == NULL)
-     {
-         fclose(source);
-         printf("Não foi possível criar %s.\n", argv[2]);
-         return 1;
-     }
+
  // Ler os primeiros 3 bytes
     BYTE bytes[3];
     fread(bytes, sizeof(BYTE), 3, file);
     // Verificar os três primeiros bytes
     if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
     {
-        printf("Talvez\n");
+
+        FILE *destination = fopen(argv[2], "w");
+        if (destination == NULL)
+        {
+            fclose(source);
+            printf("Não foi possível criar %s.\n", argv[2]);
+            return 1;
+        }
     }
     else
     {
