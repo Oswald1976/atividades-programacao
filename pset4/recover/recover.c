efc145c758e8a263f2db52cc5e01e52fc9504834
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-26 13:09:11 +0000

    Sat, Aug 26, 2023, 10:09 AM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -15,7 +15,20 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
-
+ // Ler os primeiros 3 bytes
+    BYTE bytes[3];
+    fread(bytes, sizeof(BYTE), 3, file);
+    // Verificar os três primeiros bytes
+    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
+    {
+        printf("Talvez\n");
+    }
+    else
+    {
+       printf("Não\n");
+    }
+    // Fechar o arquivo
+    fclose(file);
 
 
 }
