48a8e42276056c1b5f5045ab7f872a4e1a1daec3
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-26 23:20:48 +0000

    Sat, Aug 26, 2023, 8:20 PM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -11,11 +11,6 @@
 // 1 = tamanho em byte;
 // 512 = n de bytes a serem lidos de uma so vez;
 
-
-
-
-
-
 int main(int argc, char *argv[])
 {
     if (argc < 2) // Check for invalid usage
@@ -23,7 +18,7 @@ int main(int argc, char *argv[])
         printf("O que vai ser recuperado ?!?!?!?!\n");
         return 1;
     }
-    typedef uint8_t buffer[512];
+    uint8_t buffer[512];
     int imagem , i = 0, gravando = 0;
     char img[10] = "000.jpg";
 
@@ -63,54 +58,3 @@ int main(int argc, char *argv[])
 }
 
 
- // Ler os primeiros 3 bytes
-    BYTE bytes[3];
-    fread(bytes, sizeof(BYTE), 3, file);
-    // Verificar os três primeiros bytes
-    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
-    {
-        int adest = 000;
-        const char *nomeArquivo = "%s.jpg", adest;
-        FILE *arquivo;
-        arquivo = fopen(nomeArquivo, "w");
-        if (arquivo != NULL)
-        {
-            printf("O arquivo existe.\n");
-            fclose(arquivo);
-        }
-        else
-        {
-            printf("O arquivo não existe.\n");
-        }
-
-        FILE *destination = fopen(argv[2], "w");
-        if (destination == NULL)
-        {
-            fclose(source);
-            printf("Não foi possível criar %s.\n", argv[2]);
-            return 1;
-        }
-        else
-        {
-            // Copiar um byte de cada vez do arquivo origem(source) para o arquivo destino(destination)
-            BYTE buffer;
-            while(fread(&buffer, sizeof(BYTE), 1, file))
-            {
-                fwrite(&buffer, sizeof(BYTE), 1, destination);
-            }
-
-     // Fechar os arquivos
-     fclose(source);
-     fclose(destination);
-     return 0;
-        }
-    }
-    else
-    {
-       printf("Não\n");
-    }
-    // Fechar o arquivo
-    fclose(file);
-
-
-}
