6815ed2905aa6246d054ce5cc59602996f786a5b
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-08-26 12:19:28 +0000

    Sat, Aug 26, 2023, 9:19 AM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ if (argc < 2) // Check for invalid usage
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
 }
