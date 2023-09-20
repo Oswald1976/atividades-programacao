84da4b4c6b73c0114923d3a1152695fb2521ec3d
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-09-20 08:15:03 +0000

    Wed, Sep 20, 2023, 5:15 AM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -27,7 +27,7 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    
     unsigned int hashed = hash(word);
 
     node *n = table[hashed];
