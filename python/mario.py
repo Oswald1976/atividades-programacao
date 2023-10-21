1612b8fca8f07ce2c99082fc2c9c55855b84f67d
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-10-21 06:02:33 +0000

    Sat, Oct 21, 2023, 3:02 AM -03:00

--- python/mario.py
+++ python/mario.py
@@ -2,4 +2,6 @@ from cs50 import get_string
 # solicita a altura da piramide (tem que ser de 1 a 8)
 altura = get_string ("Altura (1 a 8) ?")
 if altura < 1 or altura > 8
+    print("Numero Invalido")
+if altura > 1 and altura < 8
     
