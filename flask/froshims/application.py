0eeda8610d0f6bf6e20841c4c371e02145dd6022
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-12-11 06:55:18 +0000

    Mon, Dec 11, 2023, 3:55 AM -03:00

--- flask/froshims/application.py
+++ flask/froshims/application.py
@@ -6,6 +6,6 @@ app =  Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register" METHOD=)
 def index():
     return render_template("register.html")
