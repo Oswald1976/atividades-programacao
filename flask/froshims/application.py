b09e8363ea41fd03536d58a049cc7714c5faeefc
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-12-11 07:20:34 +0000

    Mon, Dec 11, 2023, 4:20 AM -03:00

--- flask/froshims/application.py
+++ flask/froshims/application.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app =  Flask(__name__)
 
+SPORTS =    [
+            "Flag Football",
+            "Soccer",
+            "Volleyball",
+            "Frisbee"
+            ]
+
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", )
 
 @app.route("/register", methods=["POST"])
 def register():
