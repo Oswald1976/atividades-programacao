973b28d76a65118e4c9cbd68ac40bf663ad3379a
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-12-11 07:45:54 +0000

    Mon, Dec 11, 2023, 4:45 AM -03:00

--- flask/froshims/application.py
+++ flask/froshims/application.py
@@ -15,6 +15,6 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or  request.form.get("sport") not in SPORTS:
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("register.html")
