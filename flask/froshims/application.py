a7acdd48f748ccacb9a36e650505f59443f1ec18
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-12-12 03:32:24 +0000

    Tue, Dec 12, 2023, 12:32 AM -03:00

--- flask/froshims/application.py
+++ flask/froshims/application.py
@@ -12,7 +12,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_USE_TLS"] = True
-
+mail = Mail(app)
 
 db = SQL("sqlite:///froshims.db")
 
