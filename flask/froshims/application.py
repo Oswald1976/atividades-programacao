33cd449e224ab23f02aed3e148b35a4c957d0999
Oswald1976 <106883535+Oswald1976@users.noreply.github.com>
2023-12-13 01:06:06 +0000

    Tue, Dec 12, 2023, 10:06 PM -03:00

--- flask/froshims/application.py
+++ flask/froshims/application.py
@@ -6,12 +6,12 @@ from flask_mail import Mail, Message
 
 app =  Flask(__name__)
 app.config["MAIL_DEFAULT_SENDER"] = "obonussi@hotmail.com" #os.getenv("MAIL_DEFAULT_SENDER")
-app.config["MAIL_PASSWORD"] = "Mascarado1!" #os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PASSWORD"] = "Mascarado1" #os.getenv("MAIL_PASSWORD")
 app.config["MAIL_PORT"] = 25
 app.config["MAIL_SERVER"] = "smtp.live.com."
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "obonussi@hotmail.com" #os.getenv("MAIL_USERNAME")
+# app.config["MAIL_USE_TLS"] = True
 mail = Mail(app)
 
 db = SQL("sqlite:///froshims.db")
